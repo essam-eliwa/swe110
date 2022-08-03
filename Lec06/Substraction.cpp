58f4e4493739c855c3175ea57bd4f55c98028ca1
Essam Eliwa <essam.eliwa@miuegypt.edu.eg>
2022-08-03 16:00:25 +0200

    add loop to subtraction.cpp

--- Lec06/Substraction.cpp
+++ Lec06/Substraction.cpp
@@ -2,27 +2,29 @@
 #include <ctime> // Needed for the time function
 using namespace std;
 
-int main(){
-  int num1,num2,answer;
-  srand(time(0));
-  num1 = rand() % 101;
-  num2 = rand() % 101;
-  // swap numbers when num2 is greater than num1
-    if(num1 < num2){
+int main()
+{
+    int num1, num2, answer, exit = 0;
+    srand(time(0));
+    do{
+    num1 = rand() % 101;
+    num2 = rand() % 101;
+    // swap numbers when num2 is greater than num1
+    if (num1 < num2)
+    {
         int temp = num1;
         num1 = num2;
         num2 = temp;
     }
 
-  cout << num1 << "-" << num2 << " = ";
-  cin >> answer;
+    cout << num1 << "-" << num2 << " = ";
+    cin >> answer;
 
-  if(answer == (num1-num2)){
-    cout << "Correct";
-  } else{
-    cout << "Wrong";
-  } 
+    cout << ((answer == (num1-num2)) ? "correct" : "wrong") << endl;
 
-  
-  return 0;
+    cout << "Enter -1 to exit or any other number to continue";
+    cin >> exit;
+    }while(exit!=-1);
+
+    return 0;
 }
